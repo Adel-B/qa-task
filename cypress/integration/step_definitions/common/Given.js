6e8e3a7e60efd88e63df56117b56959c8d8d38e5
Adel Bensaad <bensaad.adel@gmail.com>
2021-09-07 10:03:58 +0200

    ci(secrets): update workflow to get GitHub secrets

--- cypress/integration/step_definitions/common/Given.js
+++ cypress/integration/step_definitions/common/Given.js
@@ -7,7 +7,7 @@ Given("I navigate to the Home Page", () => {
 Given("I log in", () => {
   const name = Cypress.env("COOKIENAME");
   const value = Cypress.env("COOKIEVALUE");
-  cy.setCookie(name.toString(), value.toString(), { expiry: 10000000000 });
+  cy.setCookie(name, value, { expiry: 10000000000 });
   cy.getCookie(name).should("have.property", "value", value);
 });
 
