af6cd387ad564cbccbec6a628eca4495f8a2b3b6
Adel Bensaad <bensaad.adel@gmail.com>
2021-09-08 15:08:12 +0200

    ci: debug

--- cypress/integration/features/users/login/login.js
+++ cypress/integration/features/users/login/login.js
@@ -2,7 +2,7 @@ import { When, Then } from "cypress-cucumber-preprocessor/steps";
 
 When("I login with valid credentials", () => {
   const username = Cypress.env("username");
-  const password = Cypress.env("password");
+  const password = Cypress.env("PASSWORD");
   cy.get("#email").type(username);
   cy.get("#passwd").type(password);
 });
