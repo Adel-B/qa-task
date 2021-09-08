51f7689483d5216aa7fb604057777b46d9008f71
Adel Bensaad <bensaad.adel@gmail.com>
2021-09-08 13:23:39 +0200

    feat(login): Add test suite

---
+++ cypress/integration/features/users/login/login.js
@@ -0,0 +1,25 @@
+import { When, Then } from "cypress-cucumber-preprocessor/steps";
+
+When("I login with valid credentials", () => {
+  const username = Cypress.env("username");
+  const password = Cypress.env("password");
+  cy.get("#email").type(username);
+  cy.get("#passwd").type(password);
+});
+
+When(/^I login with ([^"]*) and ([^"]*)$/, (email, password) => {
+  if (email) {
+    cy.get("#email").type(email);
+  }
+  if (password) {
+    cy.get("#passwd").type(password);
+  }
+});
+
+Then("I should have a link to renew my password", () => {
+  cy.contains("Forgot your password?").should(
+    "have.attr",
+    "href",
+    "http://automationpractice.com/index.php?controller=password"
+  );
+});
